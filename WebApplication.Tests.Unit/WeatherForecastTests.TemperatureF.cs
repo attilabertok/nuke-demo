b2b64edf37c767f769d858558cc4adc1e09fdf3d
bat <hun.azazel@gmail.com>
2022-06-22 01:46:57 +0200

    Add unit test to WeatherForecast

---
+++ WebApplication.Tests.Unit/WeatherForecastTests.TemperatureF.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+namespace TodoApi.Tests.Unit;
+
+public partial class WeatherForecastTests
+{
+    public class TemperatureF
+    {
+        [Fact]
+        public void Should_EqualTemperatureC_When_TemperatureIsMinus40()
+        {
+            const int intersectingTemperature = -40;
+            var sut = new WeatherForecast
+            {
+                TemperatureC = intersectingTemperature
+            };
+
+            sut.TemperatureF.Should().Be(intersectingTemperature);
+        }
+    }
+}
